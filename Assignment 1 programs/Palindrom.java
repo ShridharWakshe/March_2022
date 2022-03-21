8f9224b6a646997df2d42e3e456b599dc3389aff
ShridharWakshe <100847636+ShridharWakshe@users.noreply.github.com>
2022-03-21 19:01:15 +0530

    Update Palindrom.java

--- Assignment 1 programs/Palindrom.java
+++ Assignment 1 programs/Palindrom.java
@@ -1,14 +1,23 @@
-import java.util.Scanner;
-class Palindrom
+import java.util.*;
+class Palindrome
 {
-  public static void main(String []args)
-  {   
-  Scanner a =new Scanner(System.in);
-    int b = a.nextInt();
 	
-  
-	  for(int i = 1;i<=a; i++ ){
-	  System.out.println(i);
-	  a+=1;}
-  }
-}
+	public static void main(String args[])
+	{
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter number: ");
+	int num = sc.nextInt();
+	int temp = num;
+	int rev =0;
+	while(num!=0)
+	{
+		int a = num%10;
+		rev = rev*10+a;
+		num = num/10;
+	
+	}
+	if(temp == rev)
+		System.out.println("digit of number is Palindrome");
+	else
+		System.out.println("digit of number is not Palindrome");
+}}
